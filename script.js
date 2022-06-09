94d794027df5191c257270501ba41209462c8d99
danie11z <daniel.estudando1@gmail.com>
2022-06-08 21:47:57 -0300

    Initiating JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Daniel"
+let number = 15
+let year = 2022
+let subtraction = year - number
+let addition = number + year
+console.log(addition)
+console.log('Estou mexendo no console.log')
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
